a94db2ddb86b889f5d024e2cf282ce202b3d18e2
buhumaid3 <abdulla.skydive@gmail.com>
2026-09-23 15:31:14 +0000

    Initial iOS wrap of Live Wire via Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.buhumaid3.livewire',
+  appName: 'Live Wire',
+  webDir: 'www'
+};
+
+export default config;
