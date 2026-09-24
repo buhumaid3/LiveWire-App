86d50dd50195841b7dbd0034f1ba08641309ba0e
buhumaid3 <abdulla.skydive@gmail.com>
2026-09-24 14:05:08 +0000

    Switch capacitor.config.ts to .json to avoid requiring TypeScript on CI

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.buhumaid3.livewire',
-  appName: 'Live Wire',
-  webDir: 'www'
-};
-
-export default config;
